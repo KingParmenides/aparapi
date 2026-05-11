f79584d0c10ca06fb9209026c56c446055ce577f
KingParmenides <parmenides0000@gmail.com>
2026-05-10 23:54:24 -0600

    Mangle OpenCL reserved identifiers in generated code

--- src/main/java/com/aparapi/internal/writer/BlockWriter.java
+++ src/main/java/com/aparapi/internal/writer/BlockWriter.java
@@ -80,6 +80,31 @@ public abstract class BlockWriter{
 
    public final static String arrayDimMangleSuffix = "__javaArrayDimension";
 
+   private static final Set<String> openCLReservedIdentifiers = Collections.unmodifiableSet(new HashSet<String>(
+         Arrays.asList(
+               "__constant", "__global", "__kernel", "__local", "__private",
+               "char", "char2", "char3", "char4", "char8", "char16",
+               "constant", "double", "double2", "double3", "double4", "double8", "double16",
+               "event_t", "float", "float2", "float3", "float4", "float8", "float16",
+               "global", "half", "image1d_t", "image1d_array_t", "image1d_buffer_t",
+               "image2d_t", "image2d_array_t", "image3d_t", "int", "int2", "int3", "int4",
+               "int8", "int16", "kernel", "local", "long", "long2", "long3", "long4",
+               "long8", "long16", "private", "queue_t", "read_only", "read_write",
+               "reserve_id_t", "sampler_t", "short", "short2", "short3", "short4",
+               "short8", "short16", "uchar", "uchar2", "uchar3", "uchar4", "uchar8",
+               "uchar16", "uint", "uint2", "uint3", "uint4", "uint8", "uint16",
+               "ulong", "ulong2", "ulong3", "ulong4", "ulong8", "ulong16", "unsigned",
+               "ushort", "ushort2", "ushort3", "ushort4", "ushort8", "ushort16",
+               "void", "volatile", "write_only")));
+
+   protected String getOpenCLIdentifier(String identifier) {
+      if (openCLReservedIdentifiers.contains(identifier)) {
+         return "aparapi_" + identifier;
+      }
+
+      return identifier;
+   }
+
    public abstract void write(String _string);
 
    public void writeln(String _string) {
@@ -309,7 +334,7 @@ public abstract class BlockWriter{
       in();
       newLine();
       write("return this->");
-      write(accessorVariableFieldEntry.getNameAndTypeEntry().getNameUTF8Entry().getUTF8());
+      write(getOpenCLIdentifier(accessorVariableFieldEntry.getNameAndTypeEntry().getNameUTF8Entry().getUTF8()));
       write(";");
       out();
       newLine();
@@ -417,7 +442,7 @@ public abstract class BlockWriter{
              } else {
                  final String descriptor = localVariableInfo.getVariableDescriptor();
                  write(convertType(descriptor, true, true));
-                 write(localVariableInfo.getVariableName());
+                 write(getOpenCLIdentifier(localVariableInfo.getVariableName()));
              }
          } else {
              if (assignToLocalVariable.isDeclaration()) {
@@ -431,7 +456,7 @@ public abstract class BlockWriter{
              if (localVariableInfo == null) {
                  throw new CodeGenException("outOfScope" + _instruction.getThisPC() + " = ");
              } else {
-                 write(localVariableInfo.getVariableName() + " = ");
+                 write(getOpenCLIdentifier(localVariableInfo.getVariableName()) + " = ");
              }
          }
 
@@ -481,7 +506,7 @@ public abstract class BlockWriter{
 
             NameAndTypeEntry nameAndTypeEntry = ((AccessField) load).getConstantPoolFieldEntry().getNameAndTypeEntry();
             if (isMultiDimensionalArray(nameAndTypeEntry)) {
-               String arrayName = nameAndTypeEntry.getNameUTF8Entry().getUTF8();
+               String arrayName = getOpenCLIdentifier(nameAndTypeEntry.getNameUTF8Entry().getUTF8());
                write(" * this->" + arrayName + arrayDimMangleSuffix + dim);
             }
          }
@@ -506,7 +531,7 @@ public abstract class BlockWriter{
                writeThisRef();
             }
          }
-         write(accessField.getConstantPoolFieldEntry().getNameAndTypeEntry().getNameUTF8Entry().getUTF8());
+         write(getOpenCLIdentifier(accessField.getConstantPoolFieldEntry().getNameAndTypeEntry().getNameUTF8Entry().getUTF8()));
 
       } else if (_instruction instanceof I_ARRAYLENGTH) {
 
@@ -521,7 +546,7 @@ public abstract class BlockWriter{
             dim++;
          }
          NameAndTypeEntry nameAndTypeEntry = ((AccessInstanceField) load).getConstantPoolFieldEntry().getNameAndTypeEntry();
-         final String arrayName = nameAndTypeEntry.getNameUTF8Entry().getUTF8();
+         final String arrayName = getOpenCLIdentifier(nameAndTypeEntry.getNameUTF8Entry().getUTF8());
          String dimSuffix = isMultiDimensionalArray(nameAndTypeEntry) ? Integer.toString(dim) : "";
          write("this->" + arrayName + arrayLengthMangleSuffix + dimSuffix);
       } else if (_instruction instanceof AssignToField) {
@@ -537,7 +562,7 @@ public abstract class BlockWriter{
                writeThisRef();
             }
          }
-         write(assignedField.getConstantPoolFieldEntry().getNameAndTypeEntry().getNameUTF8Entry().getUTF8());
+         write(getOpenCLIdentifier(assignedField.getConstantPoolFieldEntry().getNameAndTypeEntry().getNameUTF8Entry().getUTF8()));
          write("=");
          writeInstruction(assignedField.getValueToAssign());
       } else if (_instruction instanceof Constant<?>) {
@@ -581,13 +606,13 @@ public abstract class BlockWriter{
       } else if (_instruction instanceof AccessLocalVariable) {
          final AccessLocalVariable localVariableLoadInstruction = (AccessLocalVariable) _instruction;
          final LocalVariableInfo localVariable = localVariableLoadInstruction.getLocalVariableInfo();
-         write(localVariable.getVariableName());
+         write(getOpenCLIdentifier(localVariable.getVariableName()));
       } else if (_instruction instanceof I_IINC) {
          final I_IINC location = (I_IINC) _instruction;
          final LocalVariableInfo localVariable = location.getLocalVariableInfo();
          final int adjust = location.getAdjust();
 
-         write(localVariable.getVariableName());
+         write(getOpenCLIdentifier(localVariable.getVariableName()));
          if (adjust == 1) {
             write("++");
          } else if (adjust == -1) {
@@ -688,7 +713,7 @@ public abstract class BlockWriter{
             if (localVariableInfo == null) {
                throw new CodeGenException("outOfScope" + _instruction.getThisPC() + " = ");
             } else {
-               write(localVariableInfo.getVariableName() + " = ");
+               write(getOpenCLIdentifier(localVariableInfo.getVariableName()) + " = ");
             }
 
          }
@@ -703,7 +728,7 @@ public abstract class BlockWriter{
             throw new CodeGenException("/* we can't declare this " + convertType(localVariableInfo.getVariableDescriptor(), true, false)
                   + " here */");
          }
-         write(localVariableInfo.getVariableName());
+         write(getOpenCLIdentifier(localVariableInfo.getVariableName()));
          write("=");
          writeInstruction(inlineAssignInstruction.getRhs());
       } else if (_instruction.getByteCode().equals(ByteCode.FIELD_ARRAY_ELEMENT_ASSIGN)) {
